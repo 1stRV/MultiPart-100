d625288df8ec80d907cb2ca80e842f2b117c7896
1stRV <firstov-roma0@yandex.ru>
2018-09-24 07:42:51 +0300

    Импорт Готов с BufferReader и List<String>!!!

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -1,23 +1,57 @@
 package hello;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RestController
 public class GreetingController {
-  private String UPLOAD_DIR = "/Users/Roman/IdeaProjects/ExampleProject/complete/src/UploadDir/";
+  private String DIR = "/Users/Roman/IdeaProjects/project85/12MultiPart/src/main/java/hello";
 
   @PostMapping("/upload")
-  public void importFile(@RequestParam("file") MultipartFile file) throws IOException {
+  public List<String> importFile(@RequestParam("file") MultipartFile file) throws IOException {
     byte[] bytes = file.getBytes();
-    Path path = Paths.get(UPLOAD_DIR + file.getOriginalFilename());
+    Path path = Paths.get(DIR + file.getOriginalFilename());
     Files.write(path, bytes);
+
+    List<String> words = new ArrayList<>();
+    if (file.isEmpty()) {
+      log.error("Файл пустой");
+      throw new IOException("Файл пустой");
+    } else {
+      try {
+        BufferedReader br = new BufferedReader(new InputStreamReader(file.getInputStream()));
+        String strLine;
+        while ((strLine = br.readLine()) != null) {
+          words.add(strLine);
+        }
+      } catch (IOException e) {
+        log.error("Не удалось прочитать файл");
+        throw new IOException("Не удалось прочитать файл");
+      }
+      return words;
+    }
   }
+
+//  @GetMapping("/upload")
+//  public void exportFile(@RequestParam("type") String type) throws IOException {
+//    byte[] bytes = file.getBytes();
+//    Path path = Paths.get(DIR + file.getOriginalFilename());
+//
+//    Files.readAllBytes(path);
+//  }
 }
